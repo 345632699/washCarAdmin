b5530b736821d458e109fc01ee0d52695b1a3421
xu <345632369@qq.com>
2018-06-06 19:25:41 +0800

    第一次提交

---
+++ src/api/api.js
@@ -0,0 +1,31 @@
+import {
+  wxRequest
+} from '../utils/wxRequest';
+
+let env = "-test" //-dev 或者 -test
+const apiMall = 'https://cc.mqphp.com/'
+// const apiMall = 'http://localhost:8080/'
+
+/**
+ * 获取发现好商品接口
+ * @param  {[type]} params [description]
+ * @return {[type]}        [description]
+ */
+const getDiscoverList = (params) => wxRequest(params, apiMall + '/goods/list?cateidOne=1&cateidTwo=0&price=0&sales=2');
+
+//微信的jscode换取sessionKey
+const wxJsCode2Session = (params) => wxRequest(params, apiMall + "/api/wechat/jscode2session");
+const user2session = (params) => wxRequest(params, apiMall + "/api/wechat/user2session?jsoncallback=?");
+
+//測試連接
+const getOpenId = (params) => wxRequest(params, apiMall + "/api/v1/sample/openid");
+const decrypt = (params) => wxRequest(params, apiMall + "/api/v1/sample/decrypt");
+
+
+module.exports = {
+  getDiscoverList,
+  wxJsCode2Session,
+  user2session,
+  getOpenId,
+  decrypt,
+}
