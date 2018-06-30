7ae76641b7cdd1affbe80c2c7b5b40ef196521e1
xu <>
2018-07-01 07:26:37 +0800

    客户端  接口对接

--- src/api/api.js
+++ src/api/api.js
@@ -3,12 +3,59 @@ import {
 } from '../utils/wxRequest';
 
 let env = "-test" //-dev 或者 -test
-const apiMall = 'https://wx.cy1993.cn/cy1993_app/'
+const api = 'https://wx.cy1993.cn/cy1993_app/'
+const apiMall = 'https://wx.cy1993.cn/cy1993_app/merchant/'
 // const apiMall = 'http://localhost:8080/'
+//appid = "wx234f1e50c0aeb853"
+//secret = "64336a6964d9f53d6dc31c26fdea970c"
+const login = (params) => wxRequest(params, api + "login/");
+const getorderoverview = (params) => wxRequest(params, apiMall + "getorderoverview/");
+//获取订单列表
+const getorderlist = (params) => wxRequest(params, apiMall + "getorderlist/");
+//获取员工列表
+const getuserlist = (params) => wxRequest(params, apiMall + "getuserlist/");
+//获取订单详情
+const getorderdetails = (params) => wxRequest(params, apiMall + "getorderdetails/");
+//上传图片
+const uploadcarimage = (params) => wxRequest(params, apiMall + "uploadcarimage/");
+//获取会员等级列表
+const getviplevel = (params) => wxRequest(params, apiMall + "getviplevel/");
+//更新会员等级
+const updateviplevel = (params) => wxRequest(params, apiMall + "updateviplevel/");
+//更新会员余额
+const updatebalance = (params) => wxRequest(params, apiMall + "updatebalance/");
+//获取员工列表
+const getstafflist = (params) => wxRequest(params, apiMall + "getstafflist/");
+//获取员工信息
+const getstaffinfo = (params) => wxRequest(params, apiMall + "getstaffinfo/");
+//员工发起认证
+const updatestaffinfo = (params) => wxRequest(params, apiMall + "updatestaffinfo/");
+//获取员工审核列表
+const getstaffauthlist = (params) => wxRequest(params, apiMall + "getstaffauthlist/");
+//更新员工审核情况
+const updatestaffauth = (params) => wxRequest(params, apiMall + "updatestaffauth/");
+//更新员工职位
+const updatestaffposition = (params) => wxRequest(params, apiMall + "updatestaffposition/");
 
-const login = (params) => wxRequest(params, apiMall + "cy1993_app");
+const getbranchlist = (params) => wxRequest(params, api + "getbranchlist/");
+
+
 
 
 module.exports = {
   login,
+  getorderoverview,
+  getorderlist,
+  getuserlist,
+  getviplevel,
+  getorderdetails,
+  uploadcarimage,
+  getstafflist,
+  getstaffinfo,
+  updatestaffinfo,
+  updatebalance,
+  updateviplevel,
+  getstaffauthlist,
+  updatestaffauth,
+  getbranchlist,
 }
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -20,3 +20,5 @@ export const SYSTEM_INFO = "systemInfo";
 export const ADDRESS_ID = "addressId";
 
 export const SEL_CLASS_CODE = "selClassCode";
+
+export const APPID = "wx234f1e50c0aeb853"
